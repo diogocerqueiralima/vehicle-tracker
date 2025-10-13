5a729c7fcafbdac4152eae872dfeb65c5a08218f
Diogo Lima <diogocerqueiradelima@gmail.com>
2025-10-13 20:57:59 +0100

    feat: change protobuf message in ingestion-service and microcontroller

--- lilygo/src/encoders/LocationEncoder.h
+++ lilygo/src/encoders/LocationEncoder.h
@@ -1,7 +1,6 @@
 #include "vehicle-tracker-schemas/Location.pb.h"
 #include "microcontroller/GPS.h"
 #include "pb_encode.h"
-#include "utils/time.h"
 #include <string.h>
 
 bool string_encode_callback(pb_ostream_t *stream, const pb_field_t *field, void *const *arg) {
@@ -18,22 +17,38 @@ bool encode_location(const GPSInfo &gps, pb_ostream_t stream, size_t *encoded_si
 
     schemas_v1_location_Location msg = schemas_v1_location_Location_init_zero;
 
-    msg.timestamp = getDateTimeToUnix(gps.date, gps.utcTime);
     msg.latitude = gps.lat.toFloat();
     msg.longitude = gps.lon.toFloat();
     msg.altitude = gps.altitude;
     msg.speed = gps.speed;
     msg.heading = gps.course;
 
-    msg.latitude_hemisphere.funcs.encode = &string_encode_callback;
-    msg.longitude_hemisphere.funcs.encode = &string_encode_callback;
+    static char time_str[16];
+    static char date_str[16];
+    static char ns_str[2];
+    static char ew_str[2];
 
-    static char ns_str[2] = {0};
-    static char ew_str[2] = {0};
+    strncpy(time_str, gps.utcTime.c_str(), sizeof(time_str)-1);
+    time_str[sizeof(time_str)-1] = '\0';
+
+    strncpy(date_str, gps.date.c_str(), sizeof(date_str)-1);
+    date_str[sizeof(date_str)-1] = '\0';
+
     ns_str[0] = gps.NS;
+    ns_str[1] = '\0';
     ew_str[0] = gps.EW;
+    ew_str[1] = '\0';
 
+    msg.time.funcs.encode = &string_encode_callback;
+    msg.time.arg = (void*)time_str;
+
+    msg.date.funcs.encode = &string_encode_callback;
+    msg.date.arg = (void*)date_str;
+
+    msg.latitude_hemisphere.funcs.encode = &string_encode_callback;
     msg.latitude_hemisphere.arg = (void*)ns_str;
+
+    msg.longitude_hemisphere.funcs.encode = &string_encode_callback;
     msg.longitude_hemisphere.arg = (void*)ew_str;
 
     if (!pb_encode(&stream, schemas_v1_location_Location_fields, &msg)) {
