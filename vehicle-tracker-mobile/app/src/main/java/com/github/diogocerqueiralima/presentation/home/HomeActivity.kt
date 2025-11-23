a6f85775d77b5e8b1735e01507ab920325019154
diogocerqueiralima <diogocerqueiradelima@gmail.com>
2025-11-23 10:30:24 +0000

    feat: add mobile application structure

---
+++ vehicle-tracker-mobile/app/src/main/java/com/github/diogocerqueiralima/presentation/home/HomeActivity.kt
@@ -0,0 +1,27 @@
+package com.github.diogocerqueiralima.presentation.home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.github.diogocerqueiralima.presentation.ui.theme.VehicleTrackerMobileTheme
+
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            VehicleTrackerMobileTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text(text = "Hello World!", modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
