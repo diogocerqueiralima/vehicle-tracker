729972a530b3b945017a6672681b7ee46be41f4e
diogocerqueiralima <diogocerqueiradelima@gmail.com>
2025-12-26 17:22:33 +0000

    build: upgrade spring boot version

--- vehicle-tracker-backend/events/src/main/java/com/github/diogocerqueiralima/location/config/RabbitMQConfig.java
+++ vehicle-tracker-backend/events/src/main/java/com/github/diogocerqueiralima/location/config/RabbitMQConfig.java
@@ -1,7 +1,7 @@
 package com.github.diogocerqueiralima.location.config;
 
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,8 +20,8 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public Jackson2JsonMessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+    public JacksonJsonMessageConverter jsonMessageConverter() {
+        return new JacksonJsonMessageConverter();
     }
 
 }
