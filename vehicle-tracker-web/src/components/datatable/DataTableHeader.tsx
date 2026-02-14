9ffff26d2dc9adfeffd75b97299d505e83e95b94
Diogo Lima <diogocerqueiradelima@gmail.com>
2026-02-14 17:41:48 +0000

    feat: add InputBar component

--- vehicle-tracker-web/src/components/datatable/DataTableHeader.tsx
+++ vehicle-tracker-web/src/components/datatable/DataTableHeader.tsx
@@ -1,5 +1,8 @@
 import {DataTableContextProps} from "@/context/DataTableContext";
 import {CreateContentProps} from "@/components/datatable/DataTableContent";
+import InputBar from "@/components/InputBar";
+import {IoIosSearch} from "react-icons/io";
+import {useState} from "react";
 
 export interface CreateHeaderProps<T extends object> {
     useDataTable: () => DataTableContextProps<T>
@@ -16,13 +19,24 @@ export function createDataTableHeader<T extends object>({ useDataTable }: Create
     return function Header({ title }: HeaderProps) {
 
         const { totalItems } = useDataTable()
+        const [search, setSearch] = useState("")
 
         return (
 
-            <div className={`flex flex-row items-center gap-4 p-8`}>
+            <div className={`flex flex-row items-center justify-between p-8`}>
 
-                <h1 className={`font-bold text-xl`}> {title} </h1>
-                <span className={`text-sm px-2 py-1 rounded-xl bg-highlight`}>{ totalItems } items</span>
+                <div className={`flex flex-row gap-4`}>
+                    <h1 className={`font-bold text-xl`}> {title} </h1>
+                    <span className={`text-sm px-2 py-1 rounded-xl bg-highlight`}>{ totalItems } items</span>
+                </div>
+                
+                <InputBar
+                    icon={<IoIosSearch size={24} />}
+                    placeholder={"Procure por número de série..."}
+                    value={search}
+                    onChange={value => setSearch(value)}
+                />
+
 
             </div>
 
