2416b0db4d5405169e28ad31578857f4868182fd
Diogo Lima <diogocerqueiradelima@gmail.com>
2026-06-27 17:29:16 +0100

    fix: VehicleAssignmentFailedException or SimCardAssignmentFailedException should extend OperationFailedException

--- vehicle-tracker-backend/asset-service/src/main/java/com/github/diogocerqueiralima/asset/service/domain/exceptions/SimCardAssignmentFailedException.java
+++ vehicle-tracker-backend/asset-service/src/main/java/com/github/diogocerqueiralima/asset/service/domain/exceptions/SimCardAssignmentFailedException.java
@@ -1,13 +1,13 @@
 package com.github.diogocerqueiralima.asset.service.domain.exceptions;
 
-import com.github.diogocerqueiralima.error.common.exceptions.exceptions.ConflictException;
+import com.github.diogocerqueiralima.error.common.exceptions.exceptions.OperationFailedException;
 
 import java.util.UUID;
 
 /**
  * Exception thrown when a device-to-SIM card assignment cannot be persisted.
  */
-public class SimCardAssignmentFailedException extends ConflictException {
+public class SimCardAssignmentFailedException extends OperationFailedException {
 
     public SimCardAssignmentFailedException(UUID deviceId, UUID simCardId) {
         super("It was not possible to assign device " + deviceId + " to SIM card " + simCardId);
--- vehicle-tracker-backend/asset-service/src/main/java/com/github/diogocerqueiralima/asset/service/domain/exceptions/VehicleAssignmentFailedException.java
+++ vehicle-tracker-backend/asset-service/src/main/java/com/github/diogocerqueiralima/asset/service/domain/exceptions/VehicleAssignmentFailedException.java
@@ -1,13 +1,13 @@
 package com.github.diogocerqueiralima.asset.service.domain.exceptions;
 
-import com.github.diogocerqueiralima.error.common.exceptions.exceptions.ConflictException;
+import com.github.diogocerqueiralima.error.common.exceptions.exceptions.OperationFailedException;
 
 import java.util.UUID;
 
 /**
  * Exception thrown when a device-to-vehicle assignment cannot be persisted.
  */
-public class VehicleAssignmentFailedException extends ConflictException {
+public class VehicleAssignmentFailedException extends OperationFailedException {
 
     public VehicleAssignmentFailedException(UUID deviceId, UUID vehicleId) {
         super("It was not possible to assign device " + deviceId + " to vehicle " + vehicleId);
