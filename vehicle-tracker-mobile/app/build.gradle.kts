457faa2d80516253d46e814b6a9ae3855e354262
Diogo Lima <diogocerqueiradelima@gmail.com>
2026-07-26 20:14:13 +0100

    build: add ktor-client-auth dependency

--- vehicle-tracker-mobile/app/build.gradle.kts
+++ vehicle-tracker-mobile/app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.ktor.client.okhttp)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.auth)
     implementation(libs.kotlin.serialization)
     implementation(libs.androidx.browser)
     implementation(libs.androidx.core.ktx)
